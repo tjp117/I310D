312b2ce41b01a00c02f288c088129ae1948c9423
tjp117 <tyler.j.patton12@gmail.com>
2025-11-12 19:03:39 -0600

    volume_of_sphere added

---
+++ volume_of_sphere.py
@@ -0,0 +1,8 @@
+def calculate_volume_of_sphere(radius):
+	pi = 3.14
+	volume = (4/3)*pi*radius*radius*radius
+	return volume
+
+radius1 = 5
+volume1 = calculate_volume_of_sphere(radius1)
+print(f"The volume of circle with radius {radius1} is: {area1}")
