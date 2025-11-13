2d7a4d571505757775344fec988c2a4d82f24417
tjp117 <tyler.j.patton12@gmail.com>
2025-11-12 19:01:40 -0600

    area_of_a_circle updated

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -3,15 +3,15 @@ def compute_area_of_circle(radius):
 	area = pi * radius * radius
 	return area
 
-radius1 = 20
+radius1 = 10
 area1 = compute_area_of_circle(radius1)
 print(f"The area of circle with radius {radius1} is: {area1}")
 
-radius2 = 30
+radius2 = 20
 area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
 
-radius3 = 40
+radius3 = 30
 area3 = compute_area_of_circle(radius3)
 print(f"The area of circle with radius {radius3} is: {area3}")
 
